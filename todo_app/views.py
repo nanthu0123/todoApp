03c3fd0036c705c8e20f41812d6ceced35902d48
nanthu0123 <nanthu0123@gmail.com>
2020-08-13 23:40:13 +0530

    insert function created

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request,'home.html')
 
+def home(request):
+    return render(request, 'home.html')
+
+
+def insert(request):
+    return render(request, 'insert.html')
